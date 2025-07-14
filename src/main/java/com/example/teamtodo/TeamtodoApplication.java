140669071fde12e6ed3b71defc0b38c4bcd00f38
차익현 <shetoo22@gmail.com>
2025-07-14 17:12:34 +0900

    Initial commit

---
+++ src/main/java/com/example/teamtodo/TeamtodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamtodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamtodoApplication.class, args);
+	}
+
+}
